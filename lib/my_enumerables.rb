0a7e4bb7bb5ff1ef3ce31e42b1a3c29daf922d8d
kdoni <chrisvdoni@gmail.com>
2022-11-29 16:45:27 +0200

    I'm finally getting it.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,25 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    while index < length
+      yield(self[index], index)
+      index += 1
+    end
+    self
+  end
+
+  def my_all?
+    index = 0
+    while index < length
+      if yield(self[index]) == false
+        return false
+      else
+        index += 1
+      end
+    end
+    true
+  end
 end
 
 # You will first have to define my_each
@@ -8,6 +28,7 @@ end
 # to this method
 
 class Array
+  include Enumerable
   def my_each
     index = 0
     while index < length
